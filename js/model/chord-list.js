f6a2c0fa59b3496a97131ca93c98562adc9969cf
Charles Francoise <charles.francoise@gmail.com>
2016-08-31 22:47:34 +0200

    fix `jsdoc.conf` file - now run `jsdoc -c jsdoc.conf` to generate documentation

--- js/model/chord-list.js
+++ js/model/chord-list.js
@@ -14,6 +14,11 @@ const Chord = require('model/chord.js');
  */
 module.exports = Backbone.Collection.extend({
     model : Chord,
+    /**
+     * Compares two `Chord`s and returns `-1` if the first chord has a lower start
+     * time, `1` if the right chord has a lower start time, and `0` if start times
+     * are identical. Keeps the collection sorted by start time.
+     */
     comparator : function(left, right) {
         const leftTime = Tone.Time(left.get('start')).toTicks();
         const rightTime = Tone.Time(right.get('start')).toTicks();
