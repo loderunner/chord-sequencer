ecfa252f1117f80b051fcc9f65b21c22892766e0
Charles Francoise <charles.francoise@gmail.com>
2016-09-02 17:12:30 +0200

    B E A U T I F U L song title edit

--- js/view/song-view.js
+++ js/view/song-view.js
@@ -10,17 +10,59 @@ module.exports = Backbone.View.extend({
         this.listenTo(this.model, "change", this.render);
     },
 
-    template : _.template('<h1 class="title"><%= title %></h1>'),
+    events : {
+        'click .title' : 'clickTitle',
+        'blur .edit' : 'blurEdit',
+        'keypress .edit' : 'keyPressEdit',
+        'keyDown .edit' : 'keyDownEdit'
+    },
+
+    template : _.template('<h1 class="title"><%= title %></h1><input class="edit" value="<%= title %>">'),
 
     render : function() {
         if (this.$el.children().length === 0) {
             this.$el.append(this.template(this.model.toJSON()));
+            this.$title = this.$('.title');
+            this.$edit = this.$('.edit');
         } else {
             if (this.model.hasChanged('title')) {
-                this.$('.title').text(this.model.get('title'));
+                this.$title.text(this.model.get('title'));
             }
         }
 
         return this;
+    },
+
+    clickTitle : function() {
+        this.$edit.attr('value', this.model.get('title'));
+        this.$el.addClass('editing');
+        this.$edit.focus();
+    },
+
+    closeEdit : function(commit) {
+        if (commit) {
+            var value = this.$edit.val().trim();
+            if (value) {
+                this.model.set('title', value);
+            }
+        }
+
+        this.$el.removeClass('editing');
+    },
+
+    blurEdit : function() {
+        this.closeEdit(this.$el.hasClass('editing'));
+    },
+
+    keyPressEdit : function(e) {
+        if (e.key === "Enter") {
+            this.closeEdit(true);
+        }
+    },
+
+    keyDownEdit : function(e) {
+        if (e.key === 'Escape') {
+            this.closeEdit(false);
+        }
     }
 });
