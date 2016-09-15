39e22169e9fb12df118d8d6d8479147334344f4b
Charles Francoise <charles.francoise@gmail.com>
2016-09-15 17:38:52 +0200

    work on tests

--- js/tests/toto.js
+++ js/tests/toto.js
@@ -2,8 +2,9 @@ const Note = require('audio/tonality/note.js');
 const Scale = require('audio/tonality/scale.js');
 
 var keys = ['C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'Fb', 'E#', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B', 'Cb', 'B#'];
+console.log('{');
 for (var m in Scale.modes) {
-    console.log('*** ' + m + ' ***');
+    console.log('    "' + m + '" : {');
 
     var iv = Scale.modes[m];
     for (var k of keys) {
@@ -11,11 +12,19 @@ for (var m in Scale.modes) {
         var note = new Note(k + '0');
         for (i of iv) {
             scale.push(note.toString());
+            var prevNote = note;
             note = note.add(i);
+            if (note.letter === prevNote.letter) {
+                try {
+                    note = note.enharmonic();
+                } catch (e) {
+                }
+            }
         }
         scale.push(note.toString());
-        console.log(k + ' : ' + JSON.stringify(scale));
+        console.log('        "' + k + '" : ' + JSON.stringify(scale) + ",");
     }
-
-    console.log('\n');
+    console.log('    },\n');
 }
+
+console.log('}');
